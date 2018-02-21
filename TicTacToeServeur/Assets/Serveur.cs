372782a63b69e59276c450b082599236f7f4ca1e
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-02-21 18:47:40 -0500

    Le serveur recoit le coup du joueur et le parse

--- TicTacToeServeur/Assets/Serveur.cs
+++ TicTacToeServeur/Assets/Serveur.cs
@@ -24,8 +24,33 @@ public class Serveur : NetworkManager
 	{
         NetworkServer.Listen(PORT);
         NetworkServer.RegisterHandler(MsgType.Connect, gererConnexion);
+        NetworkServer.RegisterHandler(MsgType.Scene, recevoirMessage);
         this.enDemarrage = false;
         Debug.Log("Listen " + PORT);
+    }
+
+    // https://docs.unity3d.com/Manual/JSONSerialization.html
+    [System.Serializable]
+    public class Coup
+    {
+        public char symbole;
+        public int colonne;
+        public int rangee;
+    };
+    System.Text.RegularExpressions.Regex coquilleCoup = new System.Text.RegularExpressions.Regex("^{\"coup\":(.*)}$");
+
+    public virtual void recevoirMessage(NetworkMessage message)
+    {
+        string json = message.ReadMessage<StringMessage>().value;
+        Debug.Log("Message recu de Scene " + json);
+
+        // https://docs.unity3d.com/Manual/BestPracticeUnderstandingPerformanceInUnity5.html
+        if (json.Contains("{\"coup\":"))
+        {
+            json = coquilleCoup.Match(json).Groups[1].Value;
+            Debug.Log("Coup deballe=" + json);
+            Coup coup = JsonUtility.FromJson<Coup>(json);
+        }
     }
     public virtual void gererConnexion(NetworkMessage message)
     {
@@ -34,16 +59,17 @@ public class Serveur : NetworkManager
         {
             connexionX = message.conn;
             Debug.Log("Joueur X connecté");
-            connexionX.Send(MsgType.Scene, new StringMessage("{symbole:x}"));
+            connexionX.Send(MsgType.Scene, new StringMessage("{\"symbole\":\"x\"}"));
         }
         else if (null == connexionO)
         {
             connexionO = message.conn;
             Debug.Log("Joueur O connecté");
-            connexionO.Send(MsgType.Scene, new StringMessage("{symbole:o}"));
+            connexionO.Send(MsgType.Scene, new StringMessage("{\"symbole\":\"o\"}"));
             this.pret = true;
-            //connexionX.Send(MsgType.Scene, new StringMessage("{tour:x}"));
-            NetworkServer.SendToAll(MsgType.Scene, new StringMessage("{tour:x}"));
+            connexionX.Send(MsgType.Scene, new StringMessage("{\"tour\":\"x\"}"));
+            connexionO.Send(MsgType.Scene, new StringMessage("{\"tour\":\"x\"}"));
+            //NetworkServer.SendToAll(MsgType.Scene, new StringMessage("{tour:x}"));
         }
         else
         {
